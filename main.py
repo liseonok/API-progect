88cf9404d34d45cdcac2d48d2fd52ebbee1213b4
kushkinava <kushkinavika3@gmail.com>
2026-02-17 20:26:09 +0300

    задача 1 решена

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QPushButton
+
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QPushButton, QLabel
 from PyQt6.QtWidgets import QMainWindow
 import requests
 
 
+map_file = "map.png"
+
+
+def get_card_by_coord_and_size(lan, lot, z=15, theme="light"):
+    api_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+    url = "https://static-maps.yandex.ru/v1?"
+    params = {
+        "apikey": api_key,
+        "ll": f'{lan},{lot}',
+        "z": z,
+        "theme": theme,
+    }
+    response = requests.get(url, params=params)
+    if response:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    else:
+        print("Ошибка выполнения запроса:")
+        print(url)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        return None
+
+lan = input()
+lot = input()
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,8 +42,12 @@ class MainWindow(QMainWindow):
         self.style_button = QPushButton(self)
         self.style_button.move(40, 40)
         self.style_button.setText("Изменить тему")
-
-
+        get_card_by_coord_and_size(lan, lot)
+        self.image = QPixmap('map.png')
+        self.label = QLabel(self)
+        self.label.move(80, 60)
+        self.label.resize(600, 450)
+        self.label.setPixmap(self.image)
 
 
 if __name__ == '__main__':
@@ -25,21 +55,3 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-
-def get_card_by_coord_and_size(lan, lot, z):
-    api_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
-    url = "https://static-maps.yandex.ru/v1?"
-    params = {
-        "apikey": api_key,
-        "ll": f'{lan},{lot}',
-        "z": z
-    }
-    response = requests.get(url, params=params)
-    if response:
-        return response
-    else:
-        print("Ошибка выполнения запроса:")
-        print(url)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        return None
